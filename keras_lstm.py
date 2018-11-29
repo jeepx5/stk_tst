8bbe178171e74fe17bf9ab0d66fd912ef89ec20a
jeepx5 <jimway2002@hotmail.com>
2018-11-29 17:08:54 +0800

    Update keras_lstm.py

--- keras_lstm.py
+++ keras_lstm.py
@@ -9,7 +9,10 @@ from keras.layers import Activation
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
-
+#for ADF check
+from statsmodels.tsa.stattools import adfuller
+#for KPSS check
+from statsmodels.tsa.stattools import kpss
 
 
 df = pd.read_csv('./dataset/international-airline-passengers.csv',
@@ -17,6 +20,86 @@ df = pd.read_csv('./dataset/international-airline-passengers.csv',
 
 dataset = df.values
 dataset = dataset.astype('float32')
+print(df.head(5))
+df['Month'] = pd.date_range('1/1/1949', periods = len(dataset), freq = 'M')
+print(df.head(5))
+
+#data preprocessing
+dtrain = df
+dtrain.timestamp = pd.to_datetime(dtrain.Month, format = '%Y-%m')
+dtrain.index= dtrain.timestamp
+dtrain.drop('Month', axis = 1, inplace = True)
+#dtrain.rename(columns={'$112': 'Passenger'}, inplace = True)
+dtrain.columns = ['passenger']
+print(dtrain.head(5))
+
+'''
+结果1：两种检测均得出结论：序列是非平稳的->序列是非平稳的
+
+结果2：两种检测均得出结论：序列是平稳的->序列是平稳的
+
+结果3：KPSS =平稳；ADF =非平稳->趋势平稳，去除趋势后序列严格平稳
+
+结果4：KPSS =非平稳；ADF =平稳->差分平稳，利用差分使序列平稳。
+'''
+
+
+
+# define function for ADF test
+def adf_test(timeseries):
+    #perform Dickey-Fuller test
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag= 'AIC')
+    dfoutput = pd.Series(dftest[0:4])#,index = ['Test Statistic of Observations Used'])
+    for key, value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key]=value
+    print(dfoutput)
+
+adf_test(dtrain['passenger'])
+
+#ADF检验结果：ADF检验的统计量为1%，p值为5%，临界值为10%，置信区间为10%。
+'''f="">平稳性检验：如果检验统计量小于临界值，
+我们可以否决原假设(也就是序列是平稳的)。
+当检验统计量大于临界值时，否决原假设失败(这意味着序列不是平稳的)。'''
+
+# define function for kpss test
+def kpss_test(timesereies):
+    print('results of kpss test')
+    kpsstest = kpss(timesereies, regression = 'c')
+    kpss_output = pd.Series(kpsstest[0:3])
+    for key,value in kpsstest[3].items():
+        kpss_output['Critical Value (%s)'%key] =  value
+    print(kpss_output)
+
+kpss_test(dtrain['passenger'])
+
+'''
+KPSS检验结果：KPSS检验-检验统计量、
+p-值和临界值和置信区间分别为1%、2.5%、5%和10%'''
+'''f="">平稳性检验：如果检验统计量大于临界值，
+则否决原假设(序列不是平稳的)。
+如果检验统计量小于临界值，
+则不能否决原假设(序列是平稳的)。'''
+
+
+#符合结果4
+
+#时间序列的平稳化
+'''变换用于对方差为非常数的序列进行平稳化。
+常用的变换方法包括幂变换、平方根变换和对数变换。
+对飞机乘客数据集进行快速对数转换和差分：'''
+look_back =7
+dtrain['passenger_diff'] = dtrain['passenger']-dtrain['passenger'].shift(look_back)
+dtrain['passenger_diff'].dropna().plot()
+
+plt.show()
+
+dtrain['passenger_log'] = np.log(dtrain['passenger'])
+dtrain['passenger_log_diff'] = dtrain['passenger_log']-dtrain['passenger_log'].shift(look_back)
+dtrain['passenger_log_diff'] = np.sqrt(dtrain['passenger_log_diff'].dropna())
+dtrain['passenger_log_diff'].plot()
+plt.show()
+
 #plt.plot(dataset)
 #plt.show()
 
@@ -44,7 +127,14 @@ def create_dataset(dataset, look_back = 1):
     return np.array(dataX), np.array(dataY)
 
 
-look_back =3
+#look_back =3
+
+#ADF check
+
+
+
+
+
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
 #print(trainY, testY)
